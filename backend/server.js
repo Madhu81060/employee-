6af5e05db2a90fddc1ec0bde4c161c30546af124
Madhu81060 <annavarapumadhulatha4@gmail.com>
2026-03-25 12:18:08 +0530

    fixed gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
+require("dotenv").config(); // ✅ add this
 
 const app = express();
+
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json()); // ✅ bodyParser replace
 
 const employeeRoutes = require("./routes/employeeRoutes");
 app.use("/api/employees", employeeRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// ✅ PORT change (important for Render)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
